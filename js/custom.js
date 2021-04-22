8c54fb084540f873c2950bec97869b194bc6a95d
Arshin <arshink@co.in>
2021-04-22 11:57:58 +0530

    new update

--- js/custom.js
+++ js/custom.js
@@ -1,104 +1,38 @@
-/******************************************
-    Version: 1.0
-/****************************************** */
 
-(function($) {
-    "use strict";
 
-	// Scroll to top  		
-	if ($('#scroll-to-top').length) {
-		var scrollTrigger = 100, // px
-			backToTop = function () {
-				var scrollTop = $(window).scrollTop();
-				if (scrollTop > scrollTrigger) {
-					$('#scroll-to-top').addClass('show');
-				} else {
-					$('#scroll-to-top').removeClass('show');
-				}
-			};
-		backToTop();
-		$(window).on('scroll', function () {
-			backToTop();
-		});
-		$('#scroll-to-top').on('click', function (e) {
-			e.preventDefault();
-			$('html,body').animate({
-				scrollTop: 0
-			}, 700);
-		});
-	};
-	
-	// Banner 
-	
-    $('.heading').height( $(window).height() );
-	$('.parallaxie').parallaxie();
-	
-	
-	
-    // LOADER
-    $(window).load(function() {
-        $("#preloader").on(500).fadeOut();
-        $(".preloader").on(600).fadeOut("slow");
-    });
+  /*-------------------------------------------------------------------------------
+    PRE LOADER
+  -------------------------------------------------------------------------------*/
 
-	// Gallery Filter
-    var Container = $('.container');
-        Container.imagesLoaded(function () {
-		var portfolio = $('.gallery-menu');
-		portfolio.on('click', 'button', function () {
-			$(this).addClass('active').siblings().removeClass('active');
-			var filterValue = $(this).attr('data-filter');
-			$grid.isotope({
-				filter: filterValue
-			});
-		});
-		var $grid = $('.gallery-list').isotope({
-			itemSelector: '.gallery-grid'
-		});
+  $(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+  });
 
-    }); 
-	
-	
-    // CONTACT
-    jQuery(document).ready(function() {
-        $('#contactform').submit(function() {
-            var action = $(this).attr('action');
-            $("#message").slideUp(750, function() {
-                $('#message').hide();
-                $('#submit')
-                    .after('<img src="images/ajax-loader.gif" class="loader" />')
-                    .attr('disabled', 'disabled');
-                $.post(action, {
-                        first_name: $('#first_name').val(),
-                        last_name: $('#last_name').val(),
-                        email: $('#email').val(),
-                        phone: $('#phone').val(),
-                        select_service: $('#select_service').val(),
-                        select_price: $('#select_price').val(),
-                        comments: $('#comments').val(),
-                        verify: $('#verify').val()
-                    },
-                    function(data) {
-                        document.getElementById('message').innerHTML = data;
-                        $('#message').slideDown('slow');
-                        $('#contactform img.loader').fadeOut('slow', function() {
-                            $(this).remove()
-                        });
-                        $('#submit').removeAttr('disabled');
-                        if (data.match('success') != null) $('#contactform').slideUp('slow');
-                    }
-                );
-            });
-            return false;
-        });
-    });
-	
-	// Map
-	$(document).ready(function(){
-        $('.gmaps').gmaps();
-      });
-	  
-	
-	
 
-})(jQuery);
+  /*-------------------------------------------------------------------------------
+    jQuery Parallax
+  -------------------------------------------------------------------------------*/
+
+    function initParallax() {
+    $('#home').parallax("50%", 0.3);
+
+  }
+  initParallax();
+
+
+  /* Back top
+  -----------------------------------------------*/
+  
+  $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
